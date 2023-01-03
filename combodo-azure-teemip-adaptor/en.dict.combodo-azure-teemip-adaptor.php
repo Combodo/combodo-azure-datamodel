1aef5fc6165b8ee0408f19e70321d09cd1e23c11
xtophe38 <christophe.naud@gmail.com>
2023-01-03 11:49:26 +0100

    Integrate adaptor to extension

---
+++ combodo-azure-teemip-adaptor/en.dict.combodo-azure-teemip-adaptor.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @copyright   Copyright (C) 2010-2022 Combodo SARL
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+//
+// AzureSubnet
+//
+Dict::Add('EN US', 'English', 'English', array(
+    'Class:AzureSubnet/Attribute:ipsubnet_id' => 'Subnet',
+    'Class:AzureSubnet/Attribute:ipsubnet_id+' => '',
+));
+
+//
+// IPSubnet
+//
+Dict::Add('EN US', 'English', 'English', array(
+    'Class:IPSubnet:azureinfo' => 'Azure Information',
+    'Class:IPSubnet/Attribute:azuresubnet_id' => 'Azure Subnet',
+    'Class:IPSubnet/Attribute:azuresubnet_id+' => '',
+));
